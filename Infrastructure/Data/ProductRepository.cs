5ba44f7da3150a0ce242644ae01086fb692133fb
sokolov misha <muradovmz@gmail.com>
2020-11-08 13:11:25 +0400

    Error Handling

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
@@ -12,6 +13,11 @@ namespace Infrastructure.Data
         public ProductRepository(StoreContext context)
         {
             _context = context;
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+             return await _context.ProductBrands.ToListAsync();
         }
 
         public async Task<Product> GetProductByIdAsync(int id)
@@ -28,6 +34,11 @@ namespace Infrastructure.Data
             .Include(p=>p.ProductType)
             .Include(p=>p.ProductBrand)
             .ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
         }
     }
 }
