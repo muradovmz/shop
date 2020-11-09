0aa9e55fd025c125891390c8cd4faed6842f3ada
sokolov misha <muradovmz@gmail.com>
2020-11-09 14:21:54 +0400

    installed angular, ngx-bootstrap, font-awesome

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,20 @@ namespace Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var item in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = item.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(item.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
