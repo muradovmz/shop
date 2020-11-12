c5e2d3a0164b4b7f5a399042f1d25ddbd52d1c43
sokolov misha <muradovmz@gmail.com>
2020-11-12 15:00:40 +0400

    Routing

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { PropuctItemComponent } from './propuct-item/propuct-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 
 @NgModule({
-  declarations: [ShopComponent, PropuctItemComponent],
+  declarations: [ShopComponent, PropuctItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
-    SharedModule
-  ],
-  exports: [ShopComponent]
+    SharedModule,
+    ShopRoutingModule
+  ]
 })
 export class ShopModule { }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/Pagination';
 import { IProductType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -40,6 +41,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
